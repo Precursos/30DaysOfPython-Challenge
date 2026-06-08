801e1e04526ddea0b4a68a5d4bea2075a8852288
Precursos <precux@gmail.com>
2026-06-07 23:38:32 -0600

    ejercicios dia 1

---
+++ Day_1/helloword.py
@@ -0,0 +1,27 @@
+    # Primer comentario
+    # Segundo comentario
+    # segundo comentario
+
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
+# Dia 1: 30 Days Of Python Callenge
+
+print(2+3)      # Addition(+)
+print(3-1)      # Subtractoon(-)
+print(2*3)      # Multiplication(*)
+print(3/2)      # Division(/)
+print(3 ** 2)   # exponential(**)
+print(3 % 2)    # modulus(%)
+print(3 // 2)   # Floor division operator(//)
+
+# Comprobando tipo de datos
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Xenocaris'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Xenocaris'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
