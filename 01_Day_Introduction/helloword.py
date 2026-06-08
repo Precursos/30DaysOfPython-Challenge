ae8e0c996134c691bfa8a5d6503511ef152293af
Precursos <precux@gmail.com>
2026-06-07 23:23:40 -0600

    primer commit

---
+++ 01_Day_Introduction/helloword.py
@@ -0,0 +1,13 @@
+    # Primer comentario
+    # Segundo comentario
+    # segundo comentario
+
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
+# Day 1 30 Days Of Python Callenge
+
+print(2+3)      # Addition(+)
+print(3-1)      #Subtractoon(-)
+print(2*3)      #
