a50bc608a1cc994fbc4d427e41dba47ac86b46e0
Precursos <precux@gmail.com>
2026-06-10 03:34:01 -0600

    continuación día 2

---
+++ Day_2/variables.py
@@ -0,0 +1,13 @@
+# Variables in Python
+first_name = 'Xeno'
+last_name = 'Precursos'
+country = 'Mexico'
+age = 28
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+
+person_info = {
+   'firstname':'Xeno',
+   'lastname':'Precursos',
+   'country':'Mexico'
+   }
